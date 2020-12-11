576a029cda6d619d3121e2da8a552a659b71b3cf
Phil Fernandez <norcal618@gmail.com>
2020-12-10 23:27:52 -0800

    update remote

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Fred is a guy);
         System.out.println("\n**************************************\n");
         System.out.println("\tWelcome to TheDesk \n");
         System.out.println("**************************************");
